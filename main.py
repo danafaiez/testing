e52d06c87656f51c8ebc3010860a5c91afca0559
dana faiez <danafaiez@Arashs-iMac-2.local>
2021-08-27 13:59:08 -0700

    deleted everything and added one print statement only

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-# if __name__ == '__main__':
-a = 1
-b = 2
-
-print("hallo!")
-print("go move  your body!")
-print("go move  your body!")
-print("move")
-# todo : refactor
-
-for i in range(1):
-    u = i + 2
-    print(u)
-print('ewew')
+print("change made in main")
