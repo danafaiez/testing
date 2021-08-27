6189a42549f92b995d8c10a9c5543d8fe5046666
dana faiez <danafaiez@Arashs-iMac-2.local>
2021-08-27 10:18:53 -0700

    adding main.py for the first time

---
+++ main.py
@@ -0,0 +1,10 @@
+# if __name__ == '__main__':
+print("go move  your body!")
+print("go move  your body!")
+print("move")
+# todo : refactor
+
+for i in range(1):
+    u = i + 2
+    print(u)
+print('ewew')
